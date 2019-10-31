cd06429d00c8a6212d3b6cda8955dc367489d575
AlecDWalker <walkeralec@hotmail.com>
2019-10-31 15:49:07 +0000

    note Controller model added, first doubles implemented

--- note-controller-test.js
+++ note-controller-test.js
@@ -1,6 +1,28 @@
 function instantiateNoteController() {
-  var noteList = new NoteList();
-  var noteController = new NoteController();
-  assert.isTrue(noteController instanceof Object, 'can be instantiated')
+  function NoteControllerDouble(){};
+  var nc = new NoteControllerDouble();
+  assert.isTrue( (nc instanceof Object) === true, 'can be instantiated')
 };
 instantiateNoteController();
+
+function canUpdateInnerHTML(){
+
+  let documentDouble = {
+    getElementById: function(){
+      return elementDouble;
+    }
+  }
+
+  let elementDouble = {
+    innerHTML: "double"
+  }
+
+  var nl = new NoteList;
+  nl.addnote('Favourite drink: seltzer')
+  var nc = new NoteController();
+
+  nc.updateHTML(documentDouble)
+  assert.isTrue(elementDouble.innerHTML === '<li><div>Favourite drink: seltzer</div></li>', 'can update HTML')
+}
+
+canUpdateInnerHTML();
--- note-controller.js
+++ note-controller.js
@@ -2,16 +2,15 @@
 
 (function(exports) {
 
-  function NoteController(noteList = new NoteList){
+  function NoteController(noteList = new NoteList) {
     noteList.addnote('Favourite drink: seltzer')
     this.noteListView = new NoteListView(noteList)
   }
 
-  NoteController.prototype.updateHTML = function(){
-    var element = document.getElementById('app');
+  NoteController.prototype.updateHTML = function(doc = document){
+    var element = doc.getElementById('app');
     element.innerHTML = this.noteListView.returnHTML()
   };
 
 exports.NoteController = NoteController;
-exports.NoteList = NoteList;
 })(this);
